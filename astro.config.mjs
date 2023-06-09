2233384f8bf0cc2bb0fdc3973f1f3e4904aba94d
zackdk <958059970@qq.com>
2023-06-09 09:50:01 +0800

    依赖升级到最新

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pattern.zackdk.com/",
   integrations: [
     // Enable Preact to support Preact JSX components.
     vue(),
